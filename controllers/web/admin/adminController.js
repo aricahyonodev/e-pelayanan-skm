916202fc17beb1a27b2e298ad5069d70e88e2405
Ari Cahyono <cahyono.ari81@gmail.com>
2022-02-10 21:11:08 +0700

    Add : View Pengajuan Detail

--- controllers/web/admin/adminController.js
+++ controllers/web/admin/adminController.js
@@ -5,4 +5,7 @@ module.exports = {
   riwayat: (req, res) => {
     res.render("admin/riwayat");
   },
+  pengajuanDetail: (req, res) => {
+    res.render("admin/pengajuanDetail");
+  },
 };
--- routers/web/admin/admin.js
+++ routers/web/admin/admin.js
@@ -3,5 +3,6 @@ const Router = Express.Router();
 const adminController = require("../../../controllers/web/admin/adminController");
 Router.get("/pengajuan", adminController.pengajuan);
 Router.get("/riwayat", adminController.riwayat);
+Router.get("/pengajuan/detail", adminController.pengajuanDetail);
 
 module.exports = Router;
