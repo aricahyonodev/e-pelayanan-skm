5256bee83dc6f482ece43934f163ad2560c6e92a
Ari Cahyono <cahyono.ari81@gmail.com>
2022-02-10 08:18:04 +0700

    Add: Template Engine EJS

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const Express = require('express');
 const App = Express();
 
 // CONFIG
+// Template Engine
+App.set("view engine", "ejs");
 // Router
 const PORT = 3000; 
 App.listen(PORT, () => {
   console.log(`Express Server Running ${PORT}`);
 });
+
+App.get('/', (req, res)=> {
+    res.render('test')
+})
